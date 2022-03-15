99445790f25991c3791ce61e25edcf2fa3c185ad
WOLFRAM <81951085+aleksandrkomarov017@users.noreply.github.com>
2022-03-15 20:21:04 +0300

    Create confidenc.py

---
+++ Abot/mylibs/confidenc.py
@@ -0,0 +1,6 @@
+config = {
+    'token': 'Y9t04teOGQfNPPlYXJU-ytQIe8D7qDsJtEVnVSCyt6I',#it's not my bot's token
+    'bot': 'YOURIDEA',#name
+    'id': 000000000000000000,#bot id
+    'prefix': '='
+}
