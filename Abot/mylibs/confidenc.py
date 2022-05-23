5a91175d8901a23651e432e15729f075975b532c
WOLFRAM <81951085+aleksandrkomarov017@users.noreply.github.com>
2022-05-23 05:48:41 +0300

    Update confidenc.py

--- Abot/mylibs/confidenc.py
+++ Abot/mylibs/confidenc.py
@@ -1,5 +1,5 @@
 config = {
-    'token': 'Y9t04teOGQfNPPlYXJU-ytQIe8D7qDsJtEVnVSCyt6I',#it's not my bot's token
+    'token': 'ODE0NDc4NDM4MDM4ODk2Njky.YDecJw.co6Eg-Imi3zRugN9qWyW_Q4ZcwA',#it's not my bot's token
     'bot': 'YOURIDEA',#name
     'id': 000000000000000000,#bot id
     'prefix': '='
